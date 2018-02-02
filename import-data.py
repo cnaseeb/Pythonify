e42143e5f9ff59e927905806ea17492623473b16
Chan <cnaseeb@users.noreply.github.com>
2018-02-02 15:02:08 +0100

    Create import-data.py

---
+++ import-data.py
@@ -0,0 +1,10 @@
+# Import the 'pandas'library as 'pd'
+import pandas as pd
+
+#load in the data with 'read_csv()'
+
+
+data = pd.read_csv("~/userdata.csv")
+# please specify your path to the file accordingly
+
+print (data)
