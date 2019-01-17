6b048bf91aaac91a60b763ec2450efda3184cd80
Chan <cnaseeb@users.noreply.github.com>
2019-01-17 13:18:17 +0100

    helloworld to Python

---
+++ helloWorldToPython.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+print("Hello, World!")
+
+#function
+def sum(a, b):
+    result = a + b
+    print("result is: ", result)
+    return result
+    
+#function call and print the answer
+print("The sum is", sum(5, 6))
+
+#function
+def sub(a, b):
+    result = a - b
+    print("result is: ", result)
+    return result
+    
+#function call and print the answer
+print("The subtraction is", sub(15, 6))
+
+#function
+def division(a, b):
+    result = a / b
+    print("result is: ", result)
+    return result
+
+#function call and print the answer
+print("The division is", division(15, 6))
+
+#function
+def multiply(a, b):
+    result = a * b
+    print("result is: ", result)
+    return result
+
+#function call and print the answer
+print("The multiplication is", multiply(5, 6))
