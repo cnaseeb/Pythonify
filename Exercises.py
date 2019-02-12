ef06f6ea61699126bb061861ab7af0440344c273
Chan Naseeb <cnaseeb@users.noreply.github.com>
2019-02-12 14:49:57 +0100

    Update Exercises.py

--- Exercises.py
+++ Exercises.py
@@ -15,3 +15,23 @@ ratePerHour = input("Enter rate per hour:")
 # TypeError: can't multiply sequence by non-int of type 'str' on line 5
 grossPay = float(hrs) * float(ratePerHour)
 print("Pay:", grossPay)
+
+
+##3.1
+hrs = input("Enter Hours:")
+ratePerHour = input("Enter rate per hour:")
+try:
+    fhrs = float(hrs)
+    fratePerHour = float(ratePerHour)
+except:
+	print("Error, please enter numeric input")
+	quit()
+#print(fhrs, fratePerHour)
+#Pay = float(hrs) * float(ratePerHour)
+if fhrs > 40:
+    PayNormal = fhrs * fratePerHour
+    PayExtra = (fhrs - 40) * (fratePerHour * 0.5)
+    Pay = PayExtra + PayNormal
+else:
+    Pay = fhrs * fratePerHour
+print(Pay)
