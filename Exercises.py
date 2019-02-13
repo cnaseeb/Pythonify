5df68d344ccd131f277ce76a03ff71c7af8d344c
Chan Naseeb <cnaseeb@users.noreply.github.com>
2019-02-13 12:05:16 +0100

    Update Exercises.py

--- Exercises.py
+++ Exercises.py
@@ -99,3 +99,22 @@ def computepay(hrs,ratePerHour):
 
 p = computepay(10,20)
 print("Pay", p)
+
+### 5.1
+# print Total, count of numbers, and their average entered by a user
+# also do verification
+num = 0
+total = 0
+while True :
+  sval = input('Enter a number')
+  if sval == 'done' :
+   break
+  try:
+    fval = float(sval)
+  except:
+    print('Invalid input')
+    continue
+  num = num + 1
+  total = total + fval
+  
+print(total, num, total/num)
